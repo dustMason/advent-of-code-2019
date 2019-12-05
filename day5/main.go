2544949e44a413b00d5f90ace44804880ece46ce
Jordan Sitkin <jordan.sitkin@coinbase.com>
2019-12-04 22:15:50 -0800

    maybe cleaner i dunno

--- day5/main.go
+++ day5/main.go
@@ -22,7 +22,7 @@ func main() {
 
 func runProgram(input int, program []int) {
 	fmt.Printf("running with input %v\n", input)
-  codes := append([]int{}, program...)
+	codes := append([]int{}, program...)
 	pointer := 0
 
 	for {
@@ -31,22 +31,18 @@ func runProgram(input int, program []int) {
 		reverse(digits)
 
 		opcode := digits[0]
-		mode1 := getMode(digits, 2)
-		mode2 := getMode(digits, 3)
 
 		if opcode == 9 { // we're cheating, this should be 99
 			break
 		} else if opcode == 1 {
-			a := getVal(codes, codes[pointer+1], mode1)
-			b := getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
 			c := codes[pointer+3]
-			codes[c] = a + b
+			codes[c] = args[0] + args[1]
 			pointer += 4
 		} else if opcode == 2 {
-			a := getVal(codes, codes[pointer+1], mode1)
-			b := getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
 			c := codes[pointer+3]
-			codes[c] = a * b
+			codes[c] = args[0] * args[1]
 			pointer += 4
 		} else if opcode == 3 { // STDIN
 			a := codes[pointer+1]
@@ -57,34 +53,32 @@ func runProgram(input int, program []int) {
 			fmt.Printf("%v ", codes[a])
 			pointer += 2
 		} else if opcode == 5 { // jump-if-true
-			a := getVal(codes, codes[pointer+1], mode1)
-			if a != 0 {
-				pointer = getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
+			if args[0] != 0 {
+				pointer = args[1]
 			} else {
 				pointer += 3
 			}
 		} else if opcode == 6 { // jump-if-false
-			a := getVal(codes, codes[pointer+1], mode1)
-			if a == 0 {
-				pointer = getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
+			if args[0] == 0 {
+				pointer = args[1]
 			} else {
 				pointer += 3
 			}
 		} else if opcode == 7 { // less then
-			a := getVal(codes, codes[pointer+1], mode1)
-			b := getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
 			c := codes[pointer+3]
-			if a < b {
+			if args[0] < args[1] {
 				codes[c] = 1
 			} else {
 				codes[c] = 0
 			}
 			pointer += 4
 		} else if opcode == 8 { // equal
-			a := getVal(codes, codes[pointer+1], mode1)
-			b := getVal(codes, codes[pointer+2], mode2)
+			args := getArgs(codes, pointer, digits, 2)
 			c := codes[pointer+3]
-			if a == b {
+			if args[0] == args[1] {
 				codes[c] = 1
 			} else {
 				codes[c] = 0
@@ -97,8 +91,18 @@ func runProgram(input int, program []int) {
 	fmt.Println()
 }
 
+func getArgs(codes []int, pointer int, opcode []int, n int) []int {
+	result := []int{}
+	for i := 0; i < n; i++ {
+		mode := getMode(opcode, i+2)
+		val := getVal(codes, codes[pointer+i+1], mode)
+		result = append(result, val)
+	}
+	return result
+}
+
 func getMode(d []int, i int) int {
-	if i > len(d) - 1 {
+	if i > len(d)-1 {
 		return 0
 	}
 	return d[i]
